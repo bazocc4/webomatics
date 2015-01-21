17098f24a627516cad249ed11643d2a46fc81bc2
Andy Basuki <andy_basuki_88@yahoo.com>
2015-01-21 13:20:36 +0700

    .

--- app/Controller/AccountsController.php
+++ app/Controller/AccountsController.php
@@ -253,6 +253,15 @@ class AccountsController extends AppController {
 			$this->redirect('/admin/login');
 		}
 	}
+    
+    function clearLogs()
+    {
+        $files = glob('../tmp/logs/*'); // get all file names
+		foreach($files as $file){ // iterate files
+		  if(is_file($file) && strtolower(basename($file)) != 'empty')
+		    unlink($file); // delete file
+		}
+    }
 
 	function login() 
 	{
@@ -262,7 +271,10 @@ class AccountsController extends AppController {
 			$this->Account->id = $myAccount['id'];
 			$this->Account->saveField('last_login' , $this->getNowDate());
 			$this->Account->saveField('modified' , $myAccount['modified']); // revert back modified time...
+			// clear tmp logs file !!
+            $this->clearLogs();
 			
+            // prepare going inside the dashboard !!            
 			$pecahurl = explode('/' , $this->request->url);
 			if(strtolower($pecahurl[0]) == 'admin')
 			{
